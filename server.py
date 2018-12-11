6cc7dadf3dc776b3229220c2e93fbd59c1ed87ba
RajatC13 <theimagineer.rajat@gmail.com>
2018-12-10 21:05:08 -0800

    changes done

--- server.py
+++ server.py
@@ -9,12 +9,14 @@ class Statement:
         self.sno = val
         self.command = ""
         self.status = ""
+
         self.votes = []
         self.accepted = []
 
 class Server(DatagramProtocol):
     def __init__(self, sid):
         self.id = sid
+        self.log = []
         self.designation = ""
         self.ledger = []
         self.dbname = 'assignment3_add_'+str(self.id)+'.db'
@@ -31,47 +33,53 @@ class Server(DatagramProtocol):
         #print(stmt)
         cmdlist = stmt.split(",")
 
-        if cmdlist[2] == 'EST':
-            if(self.id == cmdlist[3]):
-                self.designation = "Primary"
+        if cmdlist[0] not in self.log:
+            if cmdlist[2] == 'EST':
+                if(self.id == cmdlist[3]):
+                    self.designation = "Primary"
+                else:
+                    self.designation = "Follower"
+            elif cmdlist[2] == 'NEW' and self.designation == "Primary":
+                str = cmdlist[0]+","+cmdlist[1]+","+"INIT"+","+self.id
+                self.transport.write(str.encode("utf-8"), ("228.0.0.5", 8005))
+            elif cmdlist[2] == 'INIT':
+                s = Statement(int(cmdlist[0]))
+                s.status = "U"
+                print(f'New Statement {cmdlist[0]} {cmdlist[1]}')
+                print(f'Voting statement {cmdlist[0]}')
+                str = cmdlist[0]+","+cmdlist[1]+","+"VOTE"+","+self.id
+                self.transport.write(str.encode("utf-8"), ("228.0.0.5", 8005))
+                self.ledger.append(s)
+            elif cmdlist[2] == 'VOTE':
+                print(f'VOTE for {cmdlist[0]} from {cmdlist[3]}')
+                for i in self.ledger:
+                    if i.sno == int(cmdlist[0]) and int(cmdlist[3]) not in i.votes:
+                        i.votes.append(int(cmdlist[3]))
+                        if len(i.votes) > 2 and i.status == "U":
+                            i.status = "A"
+                            print(f'Accepting statement {cmdlist[0]}')
+                            str = cmdlist[0]+","+cmdlist[1]+","+"ACCEPT"+","+self.id
+                            self.transport.write(str.encode("utf-8"), ("228.0.0.5", 8005))
+            elif cmdlist[2] == 'ACCEPT':
+                print(f'ACCEPT for {cmdlist[0]} from {cmdlist[3]}')
+                for i in self.ledger:
+                    if i.sno == int(cmdlist[0]) and int(cmdlist[3]) not in i.accepted:
+                        i.accepted.append(int(cmdlist[3]))
+                        if len(i.accepted) > 2 and i.status == "A":
+                            i.status = "C"
+                            self.log.append(cmdlist[0])
+                            print(f'Confirming statement {cmdlist[0]}')
+                            str = cmdlist[0]+","+cmdlist[1]+","+"CONFIRM"+","+self.id
+                            self.transport.write(str.encode("utf-8"), ("228.0.0.5", 8005))
+                            keyval = cmdlist[1].split(":")
+                            self.db.set(keyval[0], int(self.db.get(keyval[0]))+int(keyval[1]))
+            elif cmdlist[2] == 'END':
+                #print(self.db.dgetall(self.dbname))
+                self.db.dump()
             else:
-                self.designation = "Follower"
-        elif cmdlist[2] == 'NEW' and self.designation == "Primary":
-            str = cmdlist[0]+","+cmdlist[1]+","+"INIT"+","+self.id
-            self.transport.write(str.encode("utf-8"), ("228.0.0.5", 8005))
-        elif cmdlist[2] == 'INIT':
-            s = Statement(int(cmdlist[0]))
-            s.status = "U"
-            print(f'New Statement {cmdlist[0]} {cmdlist[1]}')
-            print(f'Voting statement {cmdlist[0]}')
-            str = cmdlist[0]+","+cmdlist[1]+","+"VOTE"+","+self.id
-            self.transport.write(str.encode("utf-8"), ("228.0.0.5", 8005))
-            self.ledger.append(s)
-        elif cmdlist[2] == 'VOTE':
-            print(f'VOTE for {cmdlist[0]} from {cmdlist[3]}')
-            for i in self.ledger:
-                if i.sno == int(cmdlist[0]) and int(cmdlist[3]) not in i.votes:
-                    i.votes.append(int(cmdlist[3]))
-                    if len(i.votes) > 2 and i.status == "U":
-                        i.status = "A"
-                        print(f'Accepting statement {cmdlist[0]}')
-                        str = cmdlist[0]+","+cmdlist[1]+","+"ACCEPT"+","+self.id
-                        self.transport.write(str.encode("utf-8"), ("228.0.0.5", 8005))
-        elif cmdlist[2] == 'ACCEPT':
-            print(f'ACCEPT for {cmdlist[0]} from {cmdlist[3]}')
-            for i in self.ledger:
-                if i.sno == int(cmdlist[0]) and int(cmdlist[3]) not in i.accepted:
-                    i.accepted.append(int(cmdlist[3]))
-                    if len(i.accepted) > 2 and i.status == "A":
-                        i.status = "C"
-                        print(f'Confirming statement {cmdlist[0]}')
-                        str = cmdlist[0]+","+cmdlist[1]+","+"CONFIRM"+","+self.id
-                        self.transport.write(str.encode("utf-8"), ("228.0.0.5", 8005))
-                        keyval = cmdlist[1].split(":")
-                        self.db.set(keyval[0], int(self.db.get(keyval[0]))+int(keyval[1]))
-        elif cmdlist[2] == 'END':
-            #print(self.db.dgetall(self.dbname))
-            self.db.dump()
+                pass
+        else:
+            pass
 
 
 
